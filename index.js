21cda62e72234c43d0e766ef62de62038bd87dc1
Ylana Portela <ylanaportela2000@gmail.com>
2021-07-01 14:23:58 -0300

    Adding event for the dark mode

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-window.onload = function(){
+window.onload = function () {
 
-  const title = document.querySelectorAll(".title");
-  const text = document.querySelectorAll(".text");
-  const information = document.querySelectorAll(".information");
-  const description = document.querySelectorAll(".description");
+  const body = document.querySelector(".mode-light");
 
-
-  document.querySelector("input").addEventListener('change', function(e){
-    
-    if (e.target.checked){
-      title.classList.remove("title");
-      title.classList.add("title dark-mode");
-      text.classList.remove("text");
-      text.classList.add("text dark-mode");
-      information.classList.remove("information");
-      information.classList.add("information dark-mode");
-      description.classList.remove("description");
-      description.classList.add("description dark-mode");
-    }
-  
+  document.querySelector("input").addEventListener('change', function (e) {
+    body.classList.toggle("mode-dark");
   })
 }
